a6241f95a448c8b8033a180950cfd1a5762eba7d
AimandShoot <63858554+AimandShoot@users.noreply.github.com>
2025-04-03 01:07:25 +0800

    Delete GP-S380F-poll directory

--- GP-S380F-poll/src/main/java/com/example/demo/repository/PollRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Poll;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PollRepository extends JpaRepository<Poll, Long> {
-    // Add custom query methods if needed
-} 
