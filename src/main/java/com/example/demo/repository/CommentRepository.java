c7c598138d434a0241114659a59d9434ba4b929d
Jason9773 <s1313579@live.hkmu.edu.hk>
2025-04-17 18:28:50 +0800

    Update CommentRepository.java

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,15 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Comment;
-import com.example.demo.model.Poll;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPollIdOrderByCreatedAtDesc(Long pollId);
-    List<Comment> findByUserOrderByCreatedAtDesc(User user);
+    Optional<Comment> findById(Long Id);
+    List<Comment> findByUser(User user);
 }
