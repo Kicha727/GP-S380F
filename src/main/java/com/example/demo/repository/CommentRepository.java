5e4f3c2bf0c5ef6f82ce07a0148c44878c3bf911
AimandShoot <63858554+AimandShoot@users.noreply.github.com>
2025-04-19 19:34:15 +0800

    Add files via upload

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPollIdOrderByCreatedAtDesc(Long pollId);
+    
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Comment c WHERE c.poll.id = ?1")
+    void deleteByPollId(Long pollId);
 } 
